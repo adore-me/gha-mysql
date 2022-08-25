fe53ce435ae1955e6e0dd8c284f39a4372efb34b
mihai <mih.gogu@gmail.com>
2022-08-25 15:17:15 +0300

    Remove container name

--- mysql.sh
+++ mysql.sh
@@ -10,7 +10,6 @@ NC='\033[0m'
 echo -e "${BL}INFO:${NC} Running MySql with version: ${GR}$INPUT_VERSION${NC}"
 docker run \
   --platform linux/amd64 \
-  --name mysql \
   -e MYSQL_ROOT_PASSWORD="$INPUT_ROOT_PASSWORD" \
   -e MYSQL_USER="$INPUT_USER" \
   -e MYSQL_PASSWORD="$INPUT_PASSWORD" \
